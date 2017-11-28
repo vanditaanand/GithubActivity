5d3bcac4caac99738b32326abb6ce6042acf0755
vhellendoorn <vhellendoorn@live.nl>
2017-11-28 13:54:13 -0800

    Add better name for activity image in scaffolding

--- hw4/nginx-rev/init.sh
+++ hw4/nginx-rev/init.sh
@@ -2,6 +2,6 @@
 # This shell is inside the nginx docker, and used to start off operations. 
 # IT's fired off by the dorun.sh to start things
 cd /etc/nginx
-sed -e s?www.cs.ucdavis.edu?web1:8080/GithubActivity_war/? <nginx.conf > /tmp/xxx
+sed -e s?www.cs.ucdavis.edu?web1:8080/activity/? <nginx.conf > /tmp/xxx
 cp /tmp/xxx nginx.conf
 service nginx reload 
--- hw4/nginx-rev/swap2.sh
+++ hw4/nginx-rev/swap2.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 # This shell is to swap from web1 to web2
 cd /etc/nginx
-sed -e s?web1:8080/GithubActivity_war/?web2:8080/GithubActivity_war/? <nginx.conf > /tmp/xxx
+sed -e s?web1:8080/activity/?web2:8080/activity/? <nginx.conf > /tmp/xxx
 cp /tmp/xxx nginx.conf
 service nginx reload 
